1ba5615e9d7f5227e83832eb7073fd94d7698a92
Kyle Fuller <inbox@kylefuller.co.uk>
2015-01-21 20:03:42 +0100

    Add initial Xcode project

---
+++ Mockingjay/Mockingjay.h
@@ -0,0 +1,19 @@
+//
+//  Mockingjay.h
+//  Mockingjay
+//
+//  Created by Kyle Fuller on 21/01/2015.
+//  Copyright (c) 2015 Cocode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Mockingjay.
+FOUNDATION_EXPORT double MockingjayVersionNumber;
+
+//! Project version string for Mockingjay.
+FOUNDATION_EXPORT const unsigned char MockingjayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mockingjay/PublicHeader.h>
+
+
